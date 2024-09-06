45abc7c6bd3d9fdd7a747193e55ffcc58f1a8cfb
Jakson Xavier <jackd.xavier@gmail.com>
2024-09-05 23:31:53 -0300

    feat: add `findByEmail` method to users repository interface

--- src/domain/repositories/users.repository.ts
+++ src/domain/repositories/users.repository.ts
@@ -2,6 +2,7 @@ import { User } from '../entities/user.entity';
 
 export abstract class UsersRepository {
   abstract findById(id: string): Promise<User | null>;
+  abstract findByEmail(email: string): Promise<User | null>;
   abstract create(user: User): Promise<void>;
   abstract save(user: User): Promise<void>;
   abstract delete(id: string): Promise<void>;
