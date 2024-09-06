2e2de619293ba8c2dd85e239d61bcc4cf948ad31
Jakson Xavier <jackd.xavier@gmail.com>
2024-09-05 23:27:13 -0300

    feat: add users repository interface

---
+++ src/domain/repositories/users.repository.ts
@@ -0,0 +1,8 @@
+import { User } from '../entities/user.entity';
+
+export abstract class UsersRepository {
+  abstract findById(id: string): Promise<User | null>;
+  abstract create(user: User): Promise<void>;
+  abstract save(user: User): Promise<void>;
+  abstract delete(id: string): Promise<void>;
+}
