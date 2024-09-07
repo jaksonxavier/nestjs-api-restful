382541ac24a595e92eae0161dbfa22c74e21d495
Jakson Xavier <jackd.xavier@gmail.com>
2024-09-06 23:52:58 -0300

    feat: add user presenter

---
+++ src/infra/http/presenters/user.presenter.ts
@@ -0,0 +1,11 @@
+import { User } from '@domain/entities/user.entity';
+
+export class UserPresenter {
+  static toHTTP(user: User) {
+    return {
+      id: user.id.toString(),
+      name: user.name,
+      slug: user.slug,
+    };
+  }
+}
