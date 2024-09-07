b54731987aa042295825ab28e17e54f0d677de3a
Jakson Xavier <jackd.xavier@gmail.com>
2024-09-06 23:52:32 -0300

    test: add prisma user factory

--- test/factories/make-user.factory.ts
+++ test/factories/make-user.factory.ts
@@ -1,6 +1,9 @@
 import { UniqueEntityID } from '@core/entities/unique-entity-id';
 import { User, UserProps } from '@domain/entities/user.entity';
 import { faker } from '@faker-js/faker';
+import { PrismaUserMapper } from '@infra/database/prisma/mappers/prisma-user.mapper';
+import { PrismaService } from '@infra/database/prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
 
 export function makeUser(
   override: Partial<UserProps> = {},
@@ -18,4 +21,19 @@ export function makeUser(
   );
 
   return user;
+}
+
+@Injectable()
+export class UserFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaUser(data: Partial<UserProps> = {}): Promise<User> {
+    const user = makeUser(data);
+
+    await this.prisma.user.create({
+      data: PrismaUserMapper.toPersistence(user),
+    });
+
+    return user;
+  }
 }
