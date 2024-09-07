8af31ed2e0c5d2c642eed4f5670ce7fb5c7d9690
Jakson Xavier <jackd.xavier@gmail.com>
2024-09-07 13:36:13 -0300

    test: add encrypter logic

--- test/cryptography/fake-hasher.ts
+++ test/cryptography/fake-hasher.ts
@@ -1,7 +1,12 @@
+import { HashComparer } from '@application/cryptography/hash-comparer';
 import { HashGenerator } from '@application/cryptography/hash-generator';
 
-export class FakeHasher implements HashGenerator {
+export class FakeHasher implements HashGenerator, HashComparer {
   async hash(plain: string): Promise<string> {
     return plain.concat('-hashed');
+  }
+
+  async compare(plain: string, hash: string): Promise<boolean> {
+    return plain.concat('-hashed') === hash;
   }
 }
