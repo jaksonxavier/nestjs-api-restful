5a7626ccee3026383b6511a94bfb9d51f6c6604a
Jakson Xavier <jackd.xavier@gmail.com>
2024-09-06 21:33:38 -0300

    feat: add user schema

---
+++ prisma/migrations/20240906185440_users/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_slug_key" ON "users"("slug");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_slug_idx" ON "users"("slug");
+
+-- CreateIndex
+CREATE INDEX "users_email_idx" ON "users"("email");
